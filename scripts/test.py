290ae59f2bb136bbcd3982d7587245dc39df3f3f
Oliver Batchelor <saulzar@gmail.com>
2023-03-29 03:35:48 +1300

    .

--- scripts/test.py
+++ scripts/test.py
@@ -1,10 +1,12 @@
 from turbojpeg_python import Jpeg
 import argparse
 import cv2
+import numpy as np
 
 def load_rgb(path):
   image = cv2.imread(path, cv2.IMREAD_UNCHANGED)
   return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
 
 
 
@@ -19,5 +21,10 @@ if __name__ == "__main__":
   image = load_rgb(args.input)
   jpeg = Jpeg()
 
-  result = jpeg.encode16(image)
+  # result = jpeg.encode16(image.astype(np.uint16))
+  result = jpeg.encode8(image, 96)
 
+  print(f"Encoded size: {len(result)}")
+
+  image2 = cv2.imdecode(result, cv2.IMREAD_UNCHANGED)
+  print(image2.shape)
--- turbojpeg.cc
+++ turbojpeg.cc
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <errno.h>
+
+#include <iostream>
 
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
@@ -12,84 +12,129 @@ namespace py = pybind11;
 
 
 
-
-
-
 class JpegException : public std::exception {
-  std::string context;
-  std::string err;
-
+  std::string msg;
 
   public:
-    JpegException(std::string const& _context, std::string const& _err) :
-      err(_err), context(_context)
-    { }
-        
-    const char * what () const throw () {
+    JpegException(std::string const& context, std::string const& err) { 
       std::stringstream ss;
       ss << context << ", TurboJPEG error " << err;
-      return ss.str().c_str();
-
+      msg = ss.str();
+    }
+        
+    const char * what () const throw () {
+      return msg.c_str();
     }
 };
-
 
 
 
 class Jpeg {
   public:
-  
+  tjhandle handle;
+
 
   Jpeg() {
       handle = tj3Init(TJINIT_COMPRESS);
   }
 
-  void check_error(std::string const &context) {
-    auto code = tj3GetErrorCode(handle);
-    if (-1 == code){
+  void check_error(std::string const &context, int result) {
+    if (-1 == result){
         throw JpegException(context, tj3GetErrorStr(handle));
     }
   }
 
-
-  py::array_t<uint8_t> encode12(py::array_t<uint8_t> const& input);
-  py::array_t<uint8_t> encode16(py::array_t<uint16_t> const& input);
-
+  py::array_t<uint8_t> encode8(py::array_t<uint8_t> const& input, int quality=90);
+  py::array_t<uint8_t> encode12(py::array_t<uint16_t> const& input, int quality=90);
 
 
-  tjhandle handle;
+
 };
 
-py::array_t<uint8_t> Jpeg::encode12(py::array_t<uint8_t> const& input) {
-   
-// int tj3Compress12(tjhandle handle, const short *srcBuf, int width,
-//                             int pitch, int height, int pixelFormat,
-//                             unsigned char **jpegBuf, size_t *jpegSize); 
-    
 
-    py::array_t<uint8_t> output;
+
+// int tj3Compress16(tjhandle handle, const unsigned short *srcBuf,
+//                             int width, int pitch, int height, int pixelFormat,
+//                             unsigned char **jpegBuf, size_t *jpegSize);
+py::array_t<uint8_t> Jpeg::encode12(py::array_t<uint16_t> const& input) {
+    if (input.ndim() != 3) {
+        throw std::invalid_argument("encode12: only 3d arrays supported");
+    }
+
+    if (input.shape(2) != 3) {
+        throw std::invalid_argument("encode12: only 3 channel rgb supported");
+    }    
+
+    int height = input.shape(0);
+    int width = input.shape(1);
+
+    size_t buf_size = 0;
+    unsigned char *buf_data = NULL;
+
+    tj3Set(handle, TJPARAM_QUALITY, quality);
+    tj3Set(handle, TJPARAM_SUBSAMP, TJSAMP_422);
+    tj3Set(handle, TJPARAM_LOSSLESS, 0);
+
+
+    check_error("tj3Compress12", tj3Compress12(handle, input.data(), 
+      width, 1, height, TJPF_RGB, &buf_data, &buf_size));
+
+    py::array_t<uint8_t> output({buf_size}, buf_data, py::capsule(buf_data, [](void *f) {
+        tj3Free(f);
+    }));
+
     return output;
 }
 
-py::array_t<uint8_t> Jpeg::encode16(py::array_t<uint16_t> const& input) {
-    
-    auto shape = input.shape();
-    int height = shape[0];
-    int width = shape[1];
-    int channels = shape[2];
+py::array_t<uint8_t> Jpeg::encode8(py::array_t<uint8_t> const& input, int quality) {
+    if (input.ndim() != 3) {
+        throw std::invalid_argument("encode8: only 3d arrays supported");
+    }
 
-    
-    py::array_t<uint8_t> output;
+    if (input.shape(2) != 3) {
+        throw std::invalid_argument("encode8: only 3 channel rgb supported");
+    }    
+
+    int height = input.shape(0);
+    int width = input.shape(1);
+
+    size_t buf_size = 0;
+    unsigned char *buf_data = NULL;
+
+    tj3Set(handle, TJPARAM_QUALITY, quality);
+    tj3Set(handle, TJPARAM_SUBSAMP, TJSAMP_422);
+    tj3Set(handle, TJPARAM_LOSSLESS, 0);
+
+
+    check_error("tj3Compress8", tj3Compress8(handle, input.data(), 
+      width, 1, height, TJPF_RGB, &buf_data, &buf_size));
+
+    py::array_t<uint8_t> output({buf_size}, buf_data, py::capsule(buf_data, [](void *f) {
+        tj3Free(f);
+    }));
+
     return output;
 }
 
 PYBIND11_MODULE(turbojpeg_python, m) {
   m.doc() = "turbojpeg binding"; // optional module docstring
+
   auto jpeg = py::class_<Jpeg>(m, "Jpeg");
   jpeg.def(py::init<>())
+    .def("encode8", &Jpeg::encode8)
+
     .def("encode12", &Jpeg::encode12)
     .def("encode16", &Jpeg::encode16)
     .def("__repr__", [](const Jpeg &a) { return "Jpeg"; });
+
+
+  py::enum_<TJPF>(jpeg, "InputFormat")
+    .value("RGB", TJPF_RGB)
+    .value("BGR", TJPF_BGR)
+    .value("RGBX", TJPF_RGBX)
+    .value("BGRX", TJPF_BGRX)
+    .value("GRAY", TJPF_GRAY)
+    .export_values();
 
 
   py::register_exception<JpegException>(m, "JpegException");
